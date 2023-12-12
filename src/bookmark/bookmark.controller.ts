1a797891e156e06f71ecd2aa6d2cc2b2883fc94d
Thanapoom Dechalamai <fgv5386@gmail.com>
2023-12-12 18:01:46 +0700

    update bookmark api

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { BookmarkService } from './bookmark.service';
 
 @Controller('bookmark')
 export class BookmarkController {
     constructor(private service: BookmarkService) { }
+
+    @Get('getAll')
+    findAll() {
+        return this.service.findAll();
+    }
+
+    @Get('getOne/:id')
+    findOne(@Query('id') id: string) {
+        return this.service.findOne(id);
+    }
+
+
 }
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -7,5 +7,27 @@ export class BookmarkService {
     constructor(
         @InjectModel(Bookmark)
         private bookmarkModel: typeof Bookmark
+
+
     ) { }
+
+    findAll() {
+        return 'find all';
+    }
+
+    findOne(id: string) {
+        return 'find one';
+    }
+
+    create() {
+        return 'create';
+    }
+
+    update() {
+        return 'update';
+    }
+
+    delete() {
+        return 'delete';
+    }
 }
